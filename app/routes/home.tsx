517580eac7e4dbd33994cc2d8ed2ea635f8d804a
Nikita Gabetc <nikita.gabetc@gmail.com>
2025-10-14 22:17:23 +0300

    add resume card

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,34 @@
+import NavBar from "~/components/NavBar";
+import ResumeCard from "~/components/ResumeCard";
+import { resumes } from "~/constants";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+	return [
+		{ title: "Ai Resume Analyzer" },
+		{ name: "description", content: "Welcome to Ai Resume Analyzer!" },
+	];
 }
 
 export default function Home() {
-  return <Welcome />;
+	return (
+		<main className="bg-[url('/images/bg-main.svg')] bg-cover">
+			<NavBar />
+
+			<section className="main-section">
+				<div className="page-heading py-16">
+					<h1>Track Your Applications & Resume Ratings</h1>
+					<h2>Review your submissions and check AI-powered feedback.</h2>
+				</div>
+			</section>
+
+			{!!resumes?.length && (
+				<div className="resumes-section">
+					{resumes?.map((item) => (
+						<ResumeCard key={item.id} resume={item} />
+					))}
+				</div>
+			)}
+		</main>
+	);
 }
