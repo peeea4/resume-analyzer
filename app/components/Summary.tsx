030e5274ee2cc1281031ab192fb78fc04a792e7f
Nikita Gabetc <nikita.gabetc@gmail.com>
2025-10-17 10:21:52 +0300

    add resume feedback components

--- app/components/Summary.tsx
+++ app/components/Summary.tsx
@@ -1,11 +1,53 @@
 import React, { type FC } from "react";
+import ScoreGauge from "./ScoreGauge";
+import ScoreBadge from "./ScoreBadge";
 
-type Props = {
+interface ICategoryProps {
+	title: string;
+	score: number;
+}
+
+interface ISummaryProps {
 	feedback: Feedback;
+}
+
+const Category: FC<ICategoryProps> = ({ title, score }) => {
+	const textColor = score > 70 ? "text-green-600" : score > 49 ? "text-yellow-600" : "text-red-600";
+	return (
+		<div className="resume-summary">
+			<div className="category">
+				<div className="flex flex-row gap-2 items-center justify-center">
+					<p className="text-2xl">{title}</p>
+					<ScoreBadge score={score} />
+				</div>
+				<p className="text-2xl">
+					<span className={textColor}>{score}</span>/100
+				</p>
+			</div>
+		</div>
+	);
 };
 
-const Summary: FC<Props> = () => {
-	return <div>Summary</div>;
+const Summary: FC<ISummaryProps> = ({ feedback }) => {
+	return (
+		<div className="bg-white rounded-2xl shadow-md w-full">
+			<div className="flex flex-row item-center p-4 gap-8">
+				<ScoreGauge score={feedback.overallScore} />
+
+				<div className="flex flex-col gap-2">
+					<h2 className="text-2xl font-bold">Your Resume Score</h2>
+
+					<p className="text-sm text-gray-500">
+						This score is calculated based on the variables listed below.
+					</p>
+				</div>
+			</div>
+			<Category title="Tone & Styles" score={feedback.toneAndStyle.score} />
+			<Category title="Content" score={feedback.content.score} />
+			<Category title="Structure" score={feedback.structure.score} />
+			<Category title="Skills" score={feedback.skills.score} />
+		</div>
+	);
 };
 
 export default Summary;
